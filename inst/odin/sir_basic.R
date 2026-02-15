fd0226efdb43ff9267f47d27e9e211127468780e
OJWatson <oj.watson@hotmail.co.uk>
2026-02-15 17:04:49 +0000

    M5.0: tidy odin2 scaffold templates

--- inst/odin/sir_basic.R
+++ inst/odin/sir_basic.R
@@ -1,6 +1,22 @@
 # odin2 template: simple SIR with vaccination (scaffold)
+#
+# This template is intended as a starting point for mechanistic modelling.
+# Parameters are declared explicitly so they can be overridden at
+# instantiation/simulation time.
 
 model <- odin2::odin({
+  # Parameters
+  beta <- parameter(0.5)
+  gamma <- parameter(0.2)
+  v <- parameter(0)
+  births <- parameter(0)
+  N <- parameter(1)
+
+  # Initial conditions
+  S0 <- parameter(0)
+  I0 <- parameter(0)
+  R0 <- parameter(0)
+
   initial(S) <- S0
   initial(I) <- I0
   initial(R) <- R0
--- inst/odin/susceptible_balance.R
+++ inst/odin/susceptible_balance.R
@@ -4,7 +4,16 @@
 # Note: exact odin2 syntax may evolve; this is a scaffold.
 
 model <- odin2::odin({
+  # Parameters
+  rho <- parameter(1)
+  births <- parameter(0)
+  vaccinated <- parameter(0)
+  cases <- parameter(0)
+  S0 <- parameter(0)
+
   initial(S) <- S0
 
   update(S) <- max(0, S + births - vaccinated - cases / rho)
+
+  output(S) <- S
 })
